a85400a84689ca643ff0170121487662a0f083a3
Nick Souter <123076137+NickESouter@users.noreply.github.com>
2023-11-28 10:19:11 +0000

    Update and rename Pipeline_8.sh to Pipeline_6.sh

--- fMRIPrep_scripts/Pipeline_6.sh
+++ fMRIPrep_scripts/Pipeline_6.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#$ -N P8 #Job ID name sent to HPC cluster.
+#$ -N P6 #Job ID name sent to HPC cluster.
 #$ -pe openmp 5 #Number of requested parallel environments.
-#$ -o '/<directory root>/fMRIPrep/Pipeline_8/logs' #Folder where fMRIPrep logs files are saved out by the HPC cluster.
+#$ -o '/<directory root>/fMRIPrep/Pipeline_6/logs' #Folder where fMRIPrep logs files are saved out by the HPC cluster.
 #$ -e logs #Resquests logs.
 #$ -l m_mem_free=8G #Requested memory for HPC cluster.
 #$ -l 'h=!nodeXXX&!nodeYYY&! #A list of problematic nodes to avoid when submitting tasks.
@@ -12,10 +12,10 @@
 #be generated, (c) the designated output folder for preprocesed data, (d) the location of fMRIPrep license file,
 #and (e) the local FreeSurfer directory. With the excepton of input directory and license, each is edited to be pipeline-specific.
 DATA_DIR=/<directory root>/CNP_BIDS
-SCRATCH_DIR=/<directory root>/Pipeline_8/scratch
-OUT_DIR=/<directory root>/Pipeline_8/derivatives
+SCRATCH_DIR=/<directory root>/Pipeline_6/scratch
+OUT_DIR=/<directory root>/Pipeline_6/derivatives
 LICENSE=/<directory root>/fs_license/license.txt
-LOCAL_FREESURFER_DIR=/<directory root>/Pipeline_8/derivatives/sourcedata/freesurfer
+LOCAL_FREESURFER_DIR=/<directory root>/Pipeline_6/derivatives/sourcedata/freesurfer
 
 #Change current working directory to the input folder.
 cd ${DATA_DIR}
@@ -55,7 +55,7 @@ singularity run --cleanenv \
     --skip-bids-validation \
     --work-dir /wd \
     --omp-nthreads 1 --nthreads 5 --mem_mb 30000 \
-    --output-spaces MNI152NLin6Asym:res-1\ #P8: Volumetric output space resolution increased.
+    --output-spaces MNI152NLin6Asym:res-1\ #P6: Volumetric output space resolution increased.
     --track-carbon \
     --country-code GBR \
     --ignore slicetiming \
